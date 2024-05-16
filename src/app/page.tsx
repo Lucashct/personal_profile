a3806d75b50de9ccae4611769221c996ec70a840
Lucas Torres <lucascms11@gmail.com>
2024-05-16 19:35:34 -0300

    [FEATURE] Creating a typing animation for the seccond title label.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,14 +14,14 @@ export default function Home() {
           </div>
         </div>
         <div className={styles.presentationDiv}>
-          <p>Olá, seja bem vindo ao meu perfil profissional :)</p>
-          <p>Sou desevolvedor front end e back end com {`${new Date().getFullYear() - 2021} anos`} de experiência</p>
-          <p>E você pode encontrar meus contatos aqui!</p>
+          <p className={`${styles.line1} ${styles.typeAnimationLine1}`}>Olá, seja bem vindo ao meu perfil profissional :)</p>
+          <p className={`${styles.line2} ${styles.typeAnimationLine2}`}>Sou desevolvedor front end e back end com {`${new Date().getFullYear() - 2021} anos`} de experiência</p>
+          <p className={`${styles.lastLine} ${styles.typeAnimationLine3}`}>E você pode encontrar meus contatos aqui!</p>
         </div>
       </header>
       
       <main>
-      
+        
       </main>
     </div>
   )
